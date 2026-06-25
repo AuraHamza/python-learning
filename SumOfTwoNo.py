dc112b3196f6b534886dc1c60136c81c5bf0195c
AuraHamza <salahuddinhamza7@gmail.com>
2026-06-26 02:00:52 +0500

    Initial commit

---
+++ SumOfTwoNo.py
@@ -0,0 +1,4 @@
+a=20
+b=22
+
+print("Sum of two numbers is:",a+b)
---
+++ datatypes.py
@@ -0,0 +1,8 @@
+
+
+age=20
+name = input ("Enter Name: ")
+print(name)
+print("Age is:",age)
+
+# in python none = null in other languages
