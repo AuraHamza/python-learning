353e520e0d57bf28b12ef09b10e614b8e2289645
AuraHamza <salahuddinhamza7@gmail.com>
2026-07-03 09:34:37 +0500

    learn typecasting and loops

---
+++ loops.py
@@ -0,0 +1,9 @@
+for i in range (1,6):
+    print(i)
+print("\nNow, Printing Hello 5 times")
+for i in range (5):
+    print("Hello")
+
+print("\nFor Loop with 3 Parameters")
+for i in range(2,11,2):
+    print(i)
---
+++ typecasting.py
@@ -0,0 +1,11 @@
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+
+sum=a+b
+print("Sum of two numbers is: ", sum)
+Difference=a-b
+print("Difference of two numbers is: ", Difference)
+Product=a*b
+print("Product of two numbers is: ", Product)
+Division=a/b
+print("Division of two numbers is: ", Division)
