17a9e2689c298edab15c9cceb5f17f72a2c342f4
AuraHamza <salahuddinhamza7@gmail.com>
2026-07-20 00:08:45 +0500

    updated

---
+++ Listing.py
@@ -0,0 +1,12 @@
+#Negative Listing 
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers[-1])
+print(numbers[-2])
+
+#Positive Listing
+print("\n")
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers[1])
+print(numbers[2])
---
+++ elif.py
@@ -0,0 +1,11 @@
+def sign(num):
+    if num>0:
+        print("1")
+    elif num<0:
+        print("-1")
+    else:
+        print("0")
+
+sign(10)
+sign(-5)
+sign(0)
